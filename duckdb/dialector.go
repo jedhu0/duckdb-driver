a35276fdd7259ec5abf12c1b0a96584a7ec4910c
jedhu0 <huoshiqiu@gmail.com>
2024-03-08 18:39:32 +0800

    fix: pk

--- duckdb/dialector.go
+++ duckdb/dialector.go
@@ -164,9 +164,9 @@ func (d Dialector) DataTypeOf(field *schema.Field) string {
 			// 	t = uIntPrefix + t
 			// }
 
-			// if field.AutoIncrement {
-			// 	t += " AUTOINCREMENT"
-			// }
+			if field.PrimaryKey {
+				t += " PRIMARY KEY AUTOINCREMENT"
+			}
 
 			return t
 		}
